1f0a4483f6656ee600d40be8d03a9c83faeeabf6
Willy Arrojas <willyarrojas@gmail.com>
2023-11-05 13:38:32 +0100

    add topics until 62

--- src/main/java/com/swa/ecommerce/dao/Productrepository.java
+++
@@ -1,7 +0,0 @@
-package com.swa.ecommerce.dao;
-
-import com.swa.ecommerce.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Productrepository extends JpaRepository<Product, Long> {
-}
